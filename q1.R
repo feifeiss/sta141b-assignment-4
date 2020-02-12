6230881ca0b5ee7b0e735913e6a1c485dbf67bfa
Randy Lai <randy.cs.lai@gmail.com>
2020-02-11 22:18:54 -0800

    fix typos

--- q1.R
+++ q1.R
@@ -1,8 +1,8 @@
 # Consider the flights and airports datasets in `nycflights13`
 # Write a shiny application which
-# 1. Show a histogram of arrival delay based on user chosen origin and destination
-# 2. Show also the min, max, mean and median of corresponding arrival delay
-# You don't have to publish your app to shinyapps.io, just have to submit this file.
+# 1. shows a histogram of arrival delay based on user chosen origin and destination
+# 2. shows also statistics such as min, max, mean and median of corresponding arrival delay
+# You don't have to publish your app to shinyapps.io, just submit this file to github.
 
 
 
